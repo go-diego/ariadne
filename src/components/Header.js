8111594468b0f2b642ad8a5c00effeaf099fbe59
Diego Bernal <diego91bernal@gmail.com>
2018-12-06 14:53:28 -0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img
+                        src="https://bulma.io/images/bulma-logo-white.png"
+                        alt="Bulma: a modern CSS framework based on Flexbox"
+                        width="112"
+                        height="28"
+                    />
+                </a>
+            </div>
+        </nav>
+    );
+}
